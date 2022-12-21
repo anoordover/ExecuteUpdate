7812983bc85213f30518d41449e3c68bf076bb79
Arno Noordover <anoordover@users.noreply.github.com>
2022-12-21 21:36:47 +0100

    migrations vergeten

---
+++ ExecuteUpdateDemo/Migrations/20221221191404_Init.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExecuteUpdateDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Credits",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "NUMBER(19)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Reference = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    ReferenceDeclaration = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    DeclarationId = table.Column<long>(type: "NUMBER(19)", nullable: true),
+                    Period_StartDate = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    Period_EndDate = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Credits", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Declarations",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "NUMBER(19)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Reference = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Declarations", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Credits");
+
+            migrationBuilder.DropTable(
+                name: "Declarations");
+        }
+    }
+}
